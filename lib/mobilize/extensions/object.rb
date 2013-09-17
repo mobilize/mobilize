92d6e4355d9cbb13c9868568b3ceedf2167ce2b8
c4ssio <cassio.paesleme@gmail.com>
2013-09-17 14:50:48 -0700

    attempt the first at generalizing retries

--- lib/mobilize/extensions/object.rb
+++ lib/mobilize/extensions/object.rb
@@ -4,4 +4,27 @@ class Object
     obj = self
     obj.instance_eval(&blk)
   end
+  def send_w_retries(method_name, *args,&blk)
+    @obj = self
+    total_retries = ENV['MOB_SEND_TOTAL_RETRIES'] || 5
+    @result = nil
+    @exc = nil
+    curr_retries = 0
+    identifier = "#{@obj.to_s} #{method_name.to_s}"
+    while curr_retries < total_retries and @result.nil?
+      begin
+        @result = @obj.send(method_name,*args,&blk)
+      rescue => @exc
+        retries += 1
+        Logger.info("Failed #{identifier} with #{@exc.to_s}")
+        Logger.info("Retrying #{identifier}; #{curr_retries.to_s} of #{total_retries.to_s} time(s)")
+      end
+    end
+    if @result.nil?
+      Logger.error("Unable to #{identifier} with: #{@exc.to_s}")
+    else
+      Logger.info("Ran #{identifier} successfully with #{curr_retries} retries")
+    end
+    return @result
+  end
 end
