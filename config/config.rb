e81ef10371a38cfc3595f3c5327a187966b358aa
c4ssio <cassio.paesleme@gmail.com>
2013-10-09 17:43:48 -0700

    refactored cache, config, worker

--- config/config.rb
+++ config/config.rb
@@ -2,35 +2,42 @@ require "settingslogic"
 require 'fileutils'
 module Mobilize
   class Config < Settingslogic
-    @@dir      = File.dirname(File.expand_path(__FILE__))
-    @@home_dir = "~/.mobilize"
-    def self.home_dir;@@home_dir;end
-    @@path     = "#{@@dir}/mob.yml"
+    @@dir                     = File.dirname File.expand_path(__FILE__)
+    @@home_dir                = "~/.mobilize"
+
+    def self.home_dir
+      @@home_dir
+    end
+
+    @@path                    = "#{@@dir}/mob.yml"
 
     #load settingslogic
     source @@path if File.exists?(@@path)
+
     namespace ENV['MOBILIZE_ENV'] || "development"
 
     #takes file from samples, copies to ~/.mobilize,
     #creates symlink in config/
-    def Config.write_sample(file_name,force=nil)
-      abs_home_dir      = File.expand_path(@@home_dir)
-      source_path       = "#{@@dir}/../samples/#{file_name}"
-      config_path       = "#{@@dir}/#{file_name}"
-      target_path       = "#{abs_home_dir}/#{file_name}"
-      FileUtils.mkdir_p abs_home_dir
+    def Config.write_sample(file_name, force = nil)
+      @file_name              = file_name
+      @abs_home_dir           = File.expand_path @@home_dir
+      @source_path            = "#{@@dir}/../samples/#{@file_name}"
+      @config_path            = "#{@@dir}/#{@file_name}"
+      @target_path            = "#{@abs_home_dir}/#{@file_name}"
 
-      if (File.exists?(target_path) and force == true) or
-         !File.exists?(target_path)
-        FileUtils.cp          source_path, target_path
-        FileUtils.ln_s        target_path, config_path, force: true
-        Mobilize::Logger.info "Wrote default to #{target_path}, " +
-                              "please add environment variables accordingly"
+      FileUtils.mkdir_p         @abs_home_dir
+
+      @force_write            = (File.exists?(@target_path) and force == true)
+      if                        @force_write or !File.exists?(target_path)
+        FileUtils.cp            @source_path, @target_path
+        FileUtils.ln_s          @target_path, @config_path, force: true
+        Mobilize::Logger.info   "Wrote default to #{@target_path}, " +
+                                "please add environment variables accordingly"
       end
 
-      if !File.exists?(config_path)
-        FileUtils       .ln_s(target_path,config_path, force: true)
-        Mobilize::Logger.info "added symlink to #{target_path} in #{config_path}"
+      if                        !File.exists?(@config_path)
+        FileUtils.ln_s          @target_path, @config_path, force: true
+        Mobilize::Logger.info   "added symlink to #{@target_path} in #{@config_path}"
       end
     end
   end
--- lib/mobilize/models/work/task/cache.rb
+++ lib/mobilize/models/work/task/cache.rb
@@ -12,69 +12,73 @@ module Mobilize
     #except for the ssh path cache
     #which is <job_dir>/ssh/in
     def dir
-      @cache   = self
-      @task    = @cache.task
-      if @task.path.class == Mobilize::Ssh
-        return "#{@cache.job_dir}/ssh/stdin"
+      @cache            = self
+      @task             = @cache.task
+      @is_ssh           = @task.path.class == Mobilize::Ssh
+      if @is_ssh
+        return            "#{@cache.job_dir}/ssh/stdin"
       else
-        @ssh   = @task.user.ec2.ssh
-        return   @task.worker.abs_dir.sub("~",@ssh.home_dir)
+        @ssh            = @task.user.ec2.ssh
+        @dir            = @task.worker.abs_dir.sub("~",@ssh.home_dir)
+        return            @dir
       end
     end
 
     def parent_dir
-      @cache = self
-      return File.dirname(@cache.dir)
+      @cache            = self
+      return              File.dirname @cache.dir
     end
 
     def home_dir
-      @cache = self
-      @task  = @cache.task
-      @ssh   = @task.user.ec2.ssh
-      return   Mobilize::Config.home_dir.sub("~",@ssh.home_dir)
+      @cache            = self
+      @task             = @cache.task
+      @ssh              = @task.user.ec2.ssh
+      @home_dir         = Mobilize::Config.home_dir.sub("~",@ssh.home_dir)
+      return              @home_dir
     end
 
     def job_dir
-      @cache = self
-      @task  = @cache.task
-      return "#{@cache.home_dir}/jobs/#{@task.user.id}/#{@task.job.name}"
+      @cache            = self
+      @task             = @cache.task
+      @job_dir          = "#{@cache.home_dir}/jobs/#{@task.user.id}/#{@task.job.name}"
+      return              @job_dir
     end
 
     def create
-      @cache      = self
-      @task       = @cache.task
-      @ssh        = @task.user.ec2.ssh
+      @cache            = self
+      @task             = @cache.task
+      @ssh              = @task.user.ec2.ssh
       #clear out and regenerate cache dir
-      @ssh        .sh("mkdir -p #{@cache.dir}")
-      Logger.info "Created cache dir #{@cache.dir}"
-      return true
+      @ssh.sh           "mkdir -p #{@cache.dir}"
+      Logger.info       "Created cache dir #{@cache.dir}"
+      return            true
     end
 
     def purge
-      @cache      = self
-      @task       = @cache.task
-      @ssh        = @task.user.ec2.ssh
+      @cache            = self
+      @task             = @cache.task
+      @ssh              = @task.user.ec2.ssh
       #also purge any tarballs
-      @ssh        .sh("sudo rm -rf #{@cache.dir}*")
-      Logger.info "Purged cache dir #{@cache.dir}"
+      @ssh.sh             "sudo rm -rf #{@cache.dir}*"
+      Logger.info         "Purged cache dir #{@cache.dir}"
     end
 
     def refresh
-      @cache      = self
-      @cache      .purge
-      @cache      .create
-      Logger.info "Refreshed cache dir #{@cache.dir}"
+      @cache            = self
+      @cache.purge
+      @cache.create
+      Logger.info         "Refreshed cache dir #{@cache.dir}"
     end
 
     def unpack
-      @cache = self
-      @task = task
-      @ssh = @task.user.ec2.ssh
-      cache_file = "#{File.basename(@cache.dir)}.tar.gz"
-      @ssh        .sh("cd #{@cache.parent_dir} && " +
-                      "rm -rf #{@cache.dir} && " +
-                      "tar -zxvf #{cache_file} && rm #{cache_file}")
-      Logger.info "Unpacked cache into #{@cache.dir}"
+      @cache            = self
+      @task             = task
+      @ssh              = @task.user.ec2.ssh
+      @cache_file       = "#{File.basename(@cache.dir)}.tar.gz"
+      @ssh.sh             "cd #{@cache.parent_dir} && " +
+                          "rm -rf #{@cache.dir} && " +
+                          "tar -zxvf #{@cache_file} && rm #{@cache_file}"
+      Logger.info         "Unpacked cache into #{@cache.dir}"
     end
   end
 end
