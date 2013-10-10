b678a22a503f470ca026a6bfd9836090eba7f79f
c4ssio <cassio.paesleme@gmail.com>
2013-10-09 17:10:59 -0700

    refactoring, test passes

--- lib/mobilize/models/path/ssh.rb
+++ lib/mobilize/models/path/ssh.rb
@@ -10,12 +10,14 @@ module Mobilize
     belongs_to :ec2
 
     def input(task)
-      @task       = task
-      @worker     = @task.worker
-      @worker     .refresh
-      @worker     .purge
-      File.open("#{@worker.dir}","w") {|f| f.print(@task.input)}
-      Logger.info "wrote input into worker at #{@worker.dir}"
+      @task              = task
+      @worker            = @task.worker
+      @worker.refresh
+      @worker.purge
+      @file              = File.open @worker.dir, "w"
+      @file.print          @task.input
+      @file.close
+      Logger.info          "wrote input into worker at #{@worker.dir}"
     end
 
     def Ssh.session
@@ -23,59 +25,70 @@ module Mobilize
     end
 
     def run(task)
-      @ssh       = self
-      @task      = task
-      @ssh       .input(@task)
+      @ssh                 = self
+      @task                = task
+      @ssh.input             @task
       #deploy ssh command to cache
-      @task      .deploy
+      @task.deploy
+      @parent_dir          = @task.cache.parent_dir
       #cd to job dir and execute file from there
-      exec_cmd   = "(cd #{@task.cache.parent_dir}/ && sh stdin) > " +
-                   "#{@task.cache.parent_dir}/stdout 2> " +
-                   "#{@task.cache.parent_dir}/stderr; " +
-                   "touch #{@task.cache.parent_dir}/log; " +
-                   "echo $? > #{@task.cache.parent_dir}/exit_signal"
-      @ssh       .sh(exec_cmd)
-      return     true
+      @exec_cmd            = "(cd #{@parent_dir}/ && sh stdin) > " +
+                             "#{@parent_dir}/stdout 2> " +
+                             "#{@parent_dir}/stderr; " +
+                             "touch #{@parent_dir}/log; " +
+                             "echo $? > #{@parent_dir}/exit_signal"
+      @ssh.sh                @exec_cmd
+
+      @streams             = @ssh.streams(@task)
+      if                     @streams[:exit_signal] != "0"
+        Logger.error         @streams[:stderr]
+      end
     end
 
     def streams(task)
-      @ssh                 = self
-      @task                = task
-      delim                = "MOBILIZE_SSH_RESULT_DELIMITER"
-      stream_array         = ["stdin","stdout","stderr","exit_signal","log"]
+      @ssh                  = self
+      @task                 = task
+      @delim                = "MOBILIZE_SSH_RESULT_DELIMITER"
+      @stream_array         = ["stdin","stdout","stderr","exit_signal","log"]
 
-      result_cmd           = "cd #{@task.cache.parent_dir} && " +
-                             "array=(#{stream_array.join(" ")}) " +
-                             "&& (for each in \"${array[@]}\"; do :; " +
-                             "cat $each; echo #{delim}; done)"
+      @result_cmd           = "cd #{@task.cache.parent_dir} && " +
+                              "array=(#{stream_array.join(" ")}) " +
+                              "&& (for each in \"${array[@]}\"; do :; " +
+                              "cat $each; echo #{delim}; done)"
 
-      result_string        = @ssh.sh(result_cmd)[:stdout]
-      result_array         = result_string.split(delim).map{|stream| stream.strip}
+      @result_string        = @ssh.sh(result_cmd)[:stdout]
+      @result_array         = @result_string.split(delim).map{|stream| stream.strip}
 
-      return               {stdin:       result_array[0],
-                            stdout:      result_array[1],
-                            stderr:      result_array[2],
-                            exit_signal: result_array[3],
-                            log:         result_array[4]}
+      return                 {stdin:       @result_array[0],
+                              stdout:      @result_array[1],
+                              stderr:      @result_array[2],
+                              exit_signal: @result_array[3],
+                              log:         @result_array[4]}
     end
 
-    def sh(command,except=true)
-      @ssh     = self
-      ssh_args = {keys: [@ssh.private_key_path],paranoid: false}
-      @result  = Net::SSH.send_w_retries("start",@ssh.ec2.dns,@ssh.user_name,ssh_args) do |ssh|
-                   ssh.run(command,except)
-                 end
-      return   @result
+    def sh(command,  except =  true)
+      @ssh                  =  self
+      @ssh_args             = {keys: [@ssh.private_key_path],
+                               paranoid: false}
+
+      send_args             = ["start", @ssh.ec2.dns, @ssh.user_name, @ssh_args]
+      @result               = Net::SSH.send_w_retries(*send_args) do |ssh|
+                                ssh.run(command, except)
+                              end
+      return                  @result
     end
 
     def cp(loc_path, rem_path)
-      @ssh     = self
-      ssh_args = {keys: [@ssh.private_key_path],paranoid: false}
-      @result  = Net::SCP.send_w_retries("start",@ssh.ec2.dns,@ssh.user_name,ssh_args) do |scp|
-                   scp.upload!(loc_path,rem_path) do |ch, name, sent, total|
-                     Logger.info "#{name}: #{sent}/#{total}"
-                   end
-                 end
+      @ssh                  = self
+      @ssh_args             = {keys: [@ssh.private_key_path],
+                               paranoid: false}
+      send_args             = ["start",@ssh.ec2.dns,@ssh.user_name,@ssh_args]
+
+      @result               = Net::SCP.send_w_retries(*send_args) do |scp|
+                                scp.upload!(loc_path,rem_path) do |ch, name, sent, total|
+                                  Logger.info "#{name}: #{sent}/#{total}"
+                                end
+                              end
       return   @result
     end
   end
--- lib/mobilize/models/work/task.rb
+++ lib/mobilize/models/work/task.rb
@@ -22,11 +22,11 @@ module Mobilize
     #assign a cache and worker to task on creation
     after_create :find_or_create_worker_and_cache
     def find_or_create_worker_and_cache
-      @task       = self
-      @task       .create_worker(task_id: @task.id)
-      Logger.info "Created worker for #{@task.id}"
-      @task       .create_cache(task_id: @task.id)
-      Logger.info "Created cache for #{@task.id}"
+      @task             = self
+      @task.create_worker task_id: @task.id
+      Logger.info         "Created worker for #{@task.id}"
+      @task.create_cache  task_id: @task.id
+      Logger.info         "Created cache for #{@task.id}"
     end
 
     def user
@@ -48,11 +48,13 @@ module Mobilize
       @task = self
       return nil unless @task.subs
       @task.subs.each do |k,v|
-        @string1 = Regexp.escape(k.to_s) # escape any special characters
-        @string2 = Regexp.escape(v.to_s).gsub("/","\\/") #also need to manually escape forward slash
-        replace_cmd = "cd #{@task.worker.parent_dir} && (find . -type f \\( ! -path '*/.*' \\) | xargs sed -ie 's/#{@string1}/#{@string2}/g')"
-        replace_cmd.popen4(true)
-        Logger.info("Replaced #{@string1} with #{@string2} in #{@task.worker.parent_dir}")
+        @string1                = Regexp.escape(k.to_s) # escape any special characters
+        @string2                = Regexp.escape(v.to_s).gsub("/","\\/") #also need to manually escape forward slash
+        @replace_cmd            = "cd #{@task.worker.parent_dir} && " +
+                                  "(find . -type f \\( ! -path '*/.*' \\) | " +
+                                  "xargs sed -ie 's/#{@string1}/#{@string2}/g')"
+        @replace_cmd.popen4(true)
+        Logger.info               "Replaced #{@string1} with #{@string2} in #{@task.worker.parent_dir}"
       end
     end
 
@@ -60,52 +62,51 @@ module Mobilize
       @task                   = Task.find(task_id)
       @session                = @task.path_model.session
       if                        @session
-        @task.update_status    :started
         begin
           @path.send            @task.name, @task
+          @task.complete
         rescue               => @exc
-          @task.update_status  :failed
-          message = "Failed task #{@task.id} with #{@exc.to_s}"
-          if @task.retries < @@config.total_retries
-            Logger.info(message)
+          @task.fail
+          if                    @task.retries < @@config.max_retries
+            Logger.info         message
+            @task.retry
           else
-            Logger.error(message)
+            Logger.error        message
           end
         end
-        @task.set_status("complete")
-        Logger.info("Completed task #{@task.id}")
       else
-        @task.requeue("No session available")
+        @task.requeue           "No session available"
       end
     end
 
     def requeue(message)
-      @task = self
+      @task              = self
       #update status if the message has changed or if 
       #it has been longer than the log frequency
-      if message != @task.status_message or 
+      if                   message != @task.status_message or
         Time.now.utc > (@task.status_time + @@config.log_frequency)
-        Logger.info(@task.status_message)
+        Logger.info        @task.status_message
       end
-      Resque.enqueue(:mobilize,Task,@task.id)
+      Resque.enqueue       :mobilize,Task,@task.id
     end
 
     #take a task worker
     #and send it over to cache
     #to ensure not too many connections are opened
     def deploy
-      @task = self
-      @cache = @task.cache
-      @worker = @task.worker
+      @task                       = self
+      @cache                      = @task.cache
+      @worker                     = @task.worker
       @cache.refresh
-      "rm #{@worker.dir}.tar.gz".popen4(false)
-      Logger.info("Starting deploy for #{@task.id}")
+      @targz_rm_cmd               = "rm #{@worker.dir}.tar.gz"
+      @targz_rm_cmd.popen4(false)
+      Logger.info                   "Starting deploy for #{@task.id}"
       @task.gsub!
-      @ssh = @task.user.ec2.ssh
+      @ssh                        = @task.user.ec2.ssh
       @worker.pack
-      @ssh.cp("#{@worker.dir}.tar.gz","#{@cache.dir}.tar.gz")
-      "rm #{@worker.dir}.tar.gz".popen4(true)
-      Logger.info("Deployed #{@task.id} to cache")
+      @ssh.cp                       "#{@worker.dir}.tar.gz", "#{@cache.dir}.tar.gz"
+      @targz_rm_cmd.popen4(true)
+      Logger.info                   "Deployed #{@task.id} to cache"
       @cache.unpack
       return true
     end
@@ -113,7 +114,7 @@ module Mobilize
     #returns in, out, err, sig, log
     def streams
       @ssh = self.path
-      @ssh .streams(self)
+      @ssh.streams(self)
     end
 
   end
