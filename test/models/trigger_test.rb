df0a2f217311055801c2c8e706a07fd5ff85974c
c4ssio <cassio.paesleme@gmail.com>
2013-10-15 17:36:56 -0700

    trigger test passes

--- test/models/trigger_test.rb
+++ test/models/trigger_test.rb
@@ -7,9 +7,9 @@ class TriggerTest < MiniTest::Unit::TestCase
     @ec2                       = Mobilize::Fixture::Ec2.default     @worker_name
     @ec2_session               = Mobilize::Ec2.session
     @ec2.find_or_create_instance @ec2_session
-    @user                      = Mobilize::Fixture::User.default    @ec2
-    @job                       = Mobilize::Fixture::Job.default     @user
-    @parent_job                = Mobilize::Fixture::Job.parent      @user
+    @user                      = Mobilize::Fixture::User.default
+    @job                       = Mobilize::Fixture::Job.default     @user, @ec2
+    @parent_job                = Mobilize::Fixture::Job.parent      @user, @ec2
   end
 
   def test_tripped
@@ -18,13 +18,13 @@ class TriggerTest < MiniTest::Unit::TestCase
     @trip_methods.each        { |trip_method|
       if                         trip_method.to_s.index("parent")
         @parent_job.delete
-        @parent_job            = Mobilize::Fixture::Job.parent(@user)
+        @parent_job            = Mobilize::Fixture::Job.parent @user, @ec2
         @job.delete
-        @job                   = Mobilize::Fixture::Job.default(@user)
+        @job                   = Mobilize::Fixture::Job.default @user, @ec2
         @expected              = Mobilize::Fixture::Trigger.send(trip_method, @job, @parent_job)
       else
         @job.delete
-        @job                   = Mobilize::Fixture::Job.default(@user)
+        @job                   = Mobilize::Fixture::Job.default @user, @ec2
         @expected              = Mobilize::Fixture::Trigger.send(trip_method, @job)
       end
       Mobilize::Logger.info      "Checking Trigger #{trip_method.to_s}"
