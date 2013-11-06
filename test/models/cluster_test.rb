6d8f5ed3d4cc833ad749a3ec6d0b9d1db2b21547
c4ssio <cassio.paesleme@gmail.com>
2013-11-05 19:36:08 -0800

    cluster test works

--- test/models/cluster_test.rb
+++ test/models/cluster_test.rb
@@ -8,10 +8,20 @@ class ClusterTest < MiniTest::Unit::TestCase
     Mobilize::Cluster.perform    "install"
     Mobilize::Cluster.perform    "start"
 
-    #wait for workers to rev up
-    _resque_web_workers          = Mobilize::Cluster.resque_web_workers
     _workers_per_engine          = Mobilize.config.cluster.engines.workers.count
     _engines                     = Mobilize::Cluster.engines
+
+    _resque_web_workers          = Mobilize::Cluster.resque_web_workers
+    #wait for workers to start
+    _attempts                    = 0
+    while _resque_web_workers.length < _engines.length and _attempts <= 5
+      Mobilize::Logger.write       "waiting for workers on all engines"
+      _resque_web_workers        = Mobilize::Cluster.resque_web_workers
+      sleep 5
+      _attempts                 += 1
+    end
+
+    raise "Worker engine start failed" if _resque_web_workers.length <= _engines.length
 
     _engines.each do |_engine|
       _key                       = "ip-#{ _engine.ip }".pretty_key
@@ -24,6 +34,6 @@ class ClusterTest < MiniTest::Unit::TestCase
   end
 
   def teardown
-    #Mobilize::Cluster.perform "terminate"
+    Mobilize::Cluster.perform "terminate"
   end
 end
