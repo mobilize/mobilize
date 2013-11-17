a9955d33bc09ccddc9395cfe19d448840cfcc328
c4ssio <cassio.paesleme@gmail.com>
2013-11-17 07:50:32 -0800

    working on script test

--- test/models/script_test.rb
+++ test/models/script_test.rb
@@ -1,31 +1,22 @@
 require "test_helper"
 class ScriptTest < MiniTest::Unit::TestCase
   def setup
+    @test                      = self
     @Fixture                   = Mobilize::Fixture
-    @box                       = Mobilize::Box.find_or_create_by_name "mobilize-script-test"
     @user                      = @Fixture::User.default
+    @crontab                   = @Fixture::Crontab.default  @user
+    @cron                      = @Fixture::Cron._once       @crontab,    "script_test"
     @stdin                     = "(echo 'log this to the log' > log) && cmd"
     @script                    = Mobilize::Script.find_or_create_by stdin: @stdin
-    @script_session            = Mobilize::Script.session
-    @job                       = @Fixture::Job.default     @user, @box, "job_script_test"
-    @stage                     = @Fixture::Stage.default   @job, 1, "run"
-    @script_task               = @Fixture::Task.default    @stage, @script, @script_session,
-                                                           subs: { cmd: 'pwd' }
+    @stage                     = @Fixture::Stage.default   @cron, 1, "run"
+    @script_task               = @Fixture::Task.default    @stage, 1, @script, subs: { cmd: 'pwd' }
   end
 
   def test_run
-    @script.run                  @script_task
-    @result                    = @script.streams                @script_task
-    assert_equal                 @result[ :stdin ].strip,         @stdin.gsub( "cmd", "pwd" )
-    assert_equal                 @result[ :stdout ].strip,        @script_task.dir
-    assert_equal                 @result[ :stderr ].strip,        ""
-    assert_equal                 @result[ :exit_signal ].strip,   "0"
-    assert_equal                 @result[ :log ].strip,           "log this to the log"
-  end
-
-  def teardown
-    @box.terminate
-    @job.purge!
-    @script.delete
+    Mobilize::Log.write                   "starting github test"
+    #_start_time                         = Time.now.utc
+    Mobilize::Cli.perform                 [ "cron", "enqueue", @cron.id ]
+    #monitor logs until expected messages are complete
+    #@test.expect _start_time
   end
 end
